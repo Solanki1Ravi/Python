65ee9feb13f4b9e81cbbbcb1112aa43a616d7ab1
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-02 13:50:53 +0530

    typeCasting in python

---
+++ 03_typeCasting/main.py
@@ -0,0 +1,29 @@
+
+
+
+# There are 2 types of casting present in the python
+
+# 1. Implicit typeCasting
+
+# 2. Explicit typeCasting
+
+# Example of implicit typeCasting
+a = 2; # a is int 
+b = 3.6; # b is float
+c = a+b;
+
+
+# print(type(c))
+# print(c)
+
+# we can convert valid strings into the another data types
+
+
+
+d = "12";
+e = "44";
+f = "harry" # gives error
+
+# Explicit typeCasting
+# print(int(d)+int(e)+int(f))
+print(float(d)+float(e))
