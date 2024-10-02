b7097a1567112461d85a9d64ec0051369f73e8f3
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-02 13:50:14 +0530

    userInput in python

---
+++ 04_userInput/main.py
@@ -0,0 +1,37 @@
+
+
+# syntax:- 
+# variable = input()
+
+# by default input() give the string as output but we can typeCast that into a specific datatypes
+
+# a = input("Enter Your name ");
+
+# string as input 
+# b = input("Enter first number ")
+# c = input("Enter second number ")
+
+# typecasting the input()
+# print(int(b)+int(c))
+# print(float(b)+float(c))
+
+# e = input("Are you married? ")
+# print((e))
+
+
+b = input("Enter first number ")
+c = input("Enter second number ")
+
+# Addition
+# print(b+c)
+# print(int(b)+int(c))
+
+# subtraction
+# print(b-c) # returns error
+# print(int(b)-int(c))
+
+# Multiplication
+# print(b*c) # returns error
+
+# Division
+# print(b/c)  # returns error
