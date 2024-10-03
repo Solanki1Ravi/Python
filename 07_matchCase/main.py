4677163a5f49b2e6db4d8eaebb75146c7283aa55
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-03 11:07:42 +0530

    matchCase in python

---
+++ 07_matchCase/main.py
@@ -0,0 +1,38 @@
+
+'''
+x = int(input("Enter a day (1 to 7): "))
+
+match x:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday") 
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Enter a valid day number")
+
+
+
+'''
+fruit = input("Enter a fruit name: ")
+match fruit:
+        case "apple":
+            print("red")
+        case "banana":
+           print("Yellow")
+        case "orange":
+            print("Orange")
+        case _:
+            print("Unknown color")
+
+# Example usage
+
