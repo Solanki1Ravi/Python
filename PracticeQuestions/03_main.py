b5dcba52bcdd5d702f88ebcac56b74bc9d5b2023
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-04 13:31:10 +0530

    practice questions of hacker rankrank

---
+++ PracticeQuestions/03_main.py
@@ -0,0 +1,15 @@
+'''
+The provided code stub reads two integers,  a and b, from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division, a // b . The second line should contain the result of float division,  a/b .
+
+No rounding or formatting is necessary.
+
+
+'''
+
+a = int(input("Enter a number "))
+b = int(input("Enter the secodn number "))
+
+print(a//b)
+print(a/b)
---
+++ PracticeQuestions/04_main.py
@@ -0,0 +1,8 @@
+'''
+The provided code stub reads and integer,n , from STDIN. For all non-negative integers i<n, print i*i .
+
+'''
+
+n = int(input("Enter a number "))
+for i in range(n):
+    print(i*i)
