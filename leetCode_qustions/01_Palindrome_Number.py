3d319628f39285cd7b88c4d34377bc0b6f99076e
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-07 09:57:14 +0530

    practice questions

---
+++ leetCode_qustions/01_Palindrome_Number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        isPalindrome = False
+        i = x
+        while(x>0):
+            x = x%10
+            
+
+
