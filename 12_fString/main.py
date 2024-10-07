996ef8170d6a5a06da21d3450bdc36fb6c70e825
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-07 09:58:06 +0530

    fstring

---
+++ 12_fString/main.py
@@ -0,0 +1,10 @@
+# Name = input("Enter Your Name: ")
+# Country = input("Enter Your Country: ")
+
+# txt = f"Hello my Name is {Name} and I'm from the {Country}"
+# print(txt)
+
+
+price = float(input("Enter the price: "))
+txt = f"The price of this item is {price:.2f}"
+print(txt)
