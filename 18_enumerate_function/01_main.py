679b31d9b054b275ef6876ad75cb8ac9dd569da5
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-07 09:58:18 +0530

    enumerate function

---
+++ 18_enumerate_function/01_main.py
@@ -0,0 +1,12 @@
+
+
+li = [10,20,30,40,50,60]
+
+# for index, liItems in enumerate(li):
+#     print(f"{index}: {liItems}")
+
+
+# changing start index 
+
+for index, liItems in enumerate(li,start=1):
+    print(f"{index}: {liItems}")
