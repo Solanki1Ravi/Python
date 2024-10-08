d4853658993571d77e8b510ecd6c0df8214a2a1c
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-08 12:18:44 +0530

    if __name__

---
+++ 19__name__/Ravi.py
@@ -0,0 +1,11 @@
+
+
+def welcome():
+    print("Welcome to the user defined module")
+
+
+if __name__ == "__main__":
+    welcome() 
+
+
+   
---
+++ 19__name__/main.py
@@ -0,0 +1,8 @@
+# The idiom is useful because it allows us to reuse code from a script by importing it as a module into another script, without running the code in the original script. 
+
+# the if __name__ == "__main__" idiom is a common pattern used in python script to determine whether the script is being run directly or being imported as a module into another script. It allows us to reuse the code from a script by importing it as a module into another script, without running the code in the original script.
+
+
+import Ravi as r 
+
+# r.welcome()
