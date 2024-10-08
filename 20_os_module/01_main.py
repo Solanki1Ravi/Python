98ec7a7c8ea1e037408faa492435d27836e884d0
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-08 12:19:06 +0530

    OS module in python

---
+++ 20_os_module/01_main.py
@@ -0,0 +1,25 @@
+# The OS module in Python provides functions for interacting with the operating system.
+
+# OS comes under Python’s standard utility modules. This module provides a portable way of using operating system-dependent functionality.
+
+
+import os as o
+
+# o.mkdir("Data")
+# print(o)
+
+# for i in range(0,6):
+#     o.mkdir(f"Data/folder_{i+1}")
+
+# cwd = o.getcwd()
+# print("Current Working Directory: ",cwd)    
+
+# print(o.chdir("D:"))
+
+
+# lisDir = o.listdir("Data/")
+# print(lisDir)
+
+
+# print(o.removedirs("Data/folder_3"))
+print(o.removedirs("Data"))
