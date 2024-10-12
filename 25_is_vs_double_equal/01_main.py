444d1555497d15cee821a6f7deabeaaa4474367a
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-12 12:04:00 +0530

    is_vs_double_equal

---
+++ 25_is_vs_double_equal/01_main.py
@@ -0,0 +1,29 @@
+'''
+1.  ==: Checks if the values are the same.
+2.  is: Checks if the objects are the same in memory.
+
+'''
+
+
+# a = [1,2,3]
+# b= [1,2,3]
+
+# print(a==b)
+# print(a is b)
+
+
+# a = {1,2,3}
+# b = {1,2,3}
+# print(type(a))
+# print(type(b))
+
+# print(a==b)
+# print(a is b)
+
+a = (1,2,3)
+b = (1,2,3)
+print(type(a))
+print(type(b))
+
+print(a==b)
+print(a is b)
