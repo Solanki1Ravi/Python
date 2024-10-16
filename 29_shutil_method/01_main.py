e6766a8de7b9a064fe5a7145e691711dab36a830
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-10-16 13:11:19 +0530

    shutil method

---
+++ 29_shutil_method/01_main.py
@@ -0,0 +1,15 @@
+import shutil as sh
+'''
+copy()--> this function copies the file located at src to a new location specified by destination. 
+
+--> if the destination location already exist , the original file be overwritten
+
+'''
+# sh.copy("D:\\GitHub-local\\Python\\29_shutil_method\\01_main.py","D:\\GitHub-local\\Python\\29_shutil_method\\02_copy_main.py")
+
+# print("i'm the main file of the shutil module folder and being copied by the copy method in shutil")
+
+
+
+# sh.copytree("29_shutil_method/myNOtes", "MyNotes")
+# sh.move("29_shutil_method/myNOtes/02_copy_main.py", "01_main_outside.py")
